d29574e20462866091376c8e1ad082eabb4d2034
Alex Martin <alexmartinturrillas@gmail.com>
2018-04-30 18:09:51 +0200

    updated with lit html

--- one-style.js
+++ one-style.js
@@ -1,22 +1,44 @@
-export const oneStyle = `
-<link href="https://fonts.googleapis.com/css?family=Lato:300" rel="stylesheet">
+import {html} from 'lit-html';
+
+export const oneStyle = html`
+<!-- <svg class="one-icon" style="width:0;height:0;position:absolute;" aria-hidden="true" focusable="false">
+  <defs>
+    <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">
+      <stop offset="0%" style="stop-color:#43CBFF;stop-opacity:1" />
+      <stop offset="100%" style="stop-color:#9708CC;stop-opacity:1" />
+    </linearGradient>
+  </defs>
+</svg> -->
 <style>
-    body {
-         font: 100% "Lato", sans-serif;
-         font-weight: 300 !important;
-         color: #333;
-         background-color: white;
-         margin: 0 !important;
-         line-height: 1.5;
-         min-height: 100vh; 
+    /** SET UP THE FOLLOWING VARIABLES IN THE INEX BODY:
+        --one-primary-color (one-color and one-background)
+        --one-secondary-color //maybe not needed, set up locally?
+        --one-border-radius
+        --one-border
+        --one-size
+        --one-font
+        --
+
+        //create a file to import in the index with the body setup
+
+
+    **/
+    * {
+        font: 100% "Lato", sans-serif;
+        font-weight: 300 !important;
+        color: #333;
      }
-    iron-icon {
+    one-icon {
         width: 55px; height: auto;
         color: blue;
         --iron-icon-height: 52px;
         --iron-icon-width: 52px;
         /*--iron-icon-fill-color: (#grad1);*/
         fill: url(#one-gradient);
+        /*fill: url(#grad1);
+        stroke: #f1f1f1;
+        stroke-width: 1px;
+        stroke-linejoin: round;*/
     }
     /*one-navbar {*/
         /*position: fixed;*/
